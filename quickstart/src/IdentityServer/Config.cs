d17e1ca4d8fe3a8b705071a5794910051cddad4d
suvarnasa <suvarnasawai@gmail.com>
2024-05-09 12:54:24 +0530

    OIDC client up and working. Can see access token claims and properties

--- quickstart/src/IdentityServer/Config.cs
+++ quickstart/src/IdentityServer/Config.cs
@@ -1,4 +1,6 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+using IdentityModel;
 
 namespace IdentityServer;
 
@@ -9,6 +11,15 @@ public static class Config
         { 
             new IdentityResources.OpenId(),
             new IdentityResources.Profile(),
+            new IdentityResource
+            {
+                Name = "verification",
+                UserClaims =  new List<string> 
+                { 
+                    JwtClaimTypes.Email,
+                    JwtClaimTypes.EmailVerified
+                }
+            }  
         };
 
     public static IEnumerable<ApiScope> ApiScopes =>
@@ -35,6 +46,29 @@ public static class Config
 
             // scopes that client has access to
             AllowedScopes = { "Api1" }
+        },
+        new Client
+        {
+            ClientId = "web",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+            
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:5002/signin-oidc" },
+            
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+            AllowedScopes =
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile,
+                "verification",
+                "Api1",
+            }
         }
+
     };
 }
